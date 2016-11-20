cfc4a77d600d5e19010939d8243b833c6f08634c
Rogiers <mrogiers@deloitte.com>
2016-11-20 11:58:22 +0100

    added initial cook and demo user

--- login/login.controller.js
+++ login/login.controller.js
@@ -5,8 +5,8 @@
         .module('app')
         .controller('LoginController', LoginController);
 
-    LoginController.$inject = ['$location', '$window', 'AuthenticationService', 'FlashService'];
-    function LoginController($location, $window, AuthenticationService, FlashService) {
+    LoginController.$inject = ['$location', '$window', 'AuthenticationService', 'FlashService', 'UserService'];
+    function LoginController($location, $window, AuthenticationService, FlashService, UserService) {
         var vm = this;
 
         vm.login = login;
@@ -20,17 +20,35 @@
             vm.dataLoading = true;
             AuthenticationService.Login(vm.username, vm.password, function (response) {
                 if (response.success) {
-                    AuthenticationService.SetCredentials(vm.username, vm.password);
+                    AuthenticationService.SetCredentials(vm.username, vm.password, response.user);
                     //$location.path('/pizza');
                     //$window.location.href = '/pizzaregistration/pizza/app/index.html';
                     $window.location.href='pizza/index.html'
-                    $window.reload();
+                    //$window.reload();
                 } else {
                     FlashService.Error(response.message);
                     vm.dataLoading = false;
                 }
             });
         };
+
+        function createInitialUsers(){
+            console.log("creating initial users");
+            var cook = {};
+            cook.firstName = "Tim";
+            cook.lastName = "Cook";
+            cook.username = "cook";
+            cook.password = "test";
+            UserService.Create(cook);
+            var demo = {};
+            demo.firstName = "Demo";
+            demo.lastName = "Test";
+            demo.username = "demo";
+            demo.password = "test";
+            UserService.Create(demo);
+        }
+
+        createInitialUsers();
     }
 
 })();

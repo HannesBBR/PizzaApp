0a3dc8d2d6df0b5a1321220872d9eacb1d9d284b
Rogiers <mrogiers@deloitte.com>
2016-11-24 18:41:18 +0000

    saving address and credit card details during pizza purchase

--- pizza/js/purchase/PurchaseCtrl.js
+++ pizza/js/purchase/PurchaseCtrl.js
@@ -1,8 +1,36 @@
 'use strict';
 
-app.controller('PurchaseCtrl', function ($scope, $location, Order) {
+app.controller('PurchaseCtrl', function ($scope, $location, $cookieStore, Order, UserService) {
 	if (Order.total === 0) {
 		$location.path('/');
 	}
 	$scope.order = Order;
+
+	$scope.user = {};
+
+	$scope.username = $cookieStore.get('globals').currentUser.username;
+
+
+	$scope.vm= [];
+
+	function loadCurrentUser() {
+		UserService.GetByUsername($scope.username)
+			.then(function (user) {
+				$scope.vm.user = user;
+				console.log("current user is: "+JSON.stringify($scope.vm.user));
+			});
+	};
+
+	function saveUser(){
+		console.log("saving user "+JSON.stringify($scope.vm.user));
+		UserService.Update($scope.vm.user);
+	}
+
+	$scope.orderPizza = function(){
+		$scope.order.purchase();
+		saveUser();
+	}
+
+	loadCurrentUser();
+
 });

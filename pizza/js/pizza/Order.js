0e982a0a4bc457c56c7fe9a6d82e12f6474b6680
Rogiers <mrogiers@deloitte.com>
2016-11-28 08:01:11 +0100

    Removed discount functionality, closes #6

--- pizza/js/pizza/Order.js
+++ pizza/js/pizza/Order.js
@@ -5,10 +5,6 @@ app.service('Order', function ($rootScope, $http) {
 
 	self.total = 0; // total of all ordered pizzas
 	self.time = 0;  // delivered time after purchase
-	self.discount = 0;
-	self.discountLevel = 5; // % level of discount
-	self.discountForTotalMoreThan = 10; // order higher than this get discountLevel
-	self.discountMessage = false;  // show discount message
 	self.cart = [];
 	self.user = {};
 
@@ -28,27 +24,15 @@ app.service('Order', function ($rootScope, $http) {
 			});
 		}
 		self.total += pizza.price;
-		if (self.total >= self.discountForTotalMoreThan) {
-			if (self.discount === 0) {
-				self.discountMessage = true;
-			}
-			self.discount = self.discountLevel;
-		}
 	};
 
 	self.remove = function (index) {
 		self.total -= self.cart[index].count * self.cart[index].pizza.price;
 		self.cart.splice(index, 1);
-		if (self.total < self.discountForTotalMoreThan) {
-			self.discount = 0;
-			self.discountMessage = false;
-		}
 	};
 
 	self.clear = function () {
 		self.total = 0;
-		self.discount = 0;
-		self.discountMessage = false;
 		self.time = 0;
 		self.cart = [];
 	};
